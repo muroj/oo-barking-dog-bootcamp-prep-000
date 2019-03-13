5110bdca89dc19378aec7a9ee3289cd746ddd1e1
Joe Muro <jmuro0321@gmail.com>
2019-03-13 17:08:46 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
